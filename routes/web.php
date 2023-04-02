b275cfebe5224065c3aedd28ff59347daf218297
Hiroki <sk38.tk924.nk826.sh1114@gmail.com>
2023-04-02 08:03:16 +0000

    PHP/laravel09

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+    Route::get('news/create', 'add');
+});
+
+//3.「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください
+Route::Controller(AAAcontroller::class)->group(function() {
+    Route::get('XXX','bbb');
+    });
+
+/*4.【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。web.phpを編集して、admin/profile/create にアクセスしたら
+ProfileController の add Action に、admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください*/
+use App\Http\controllers\Admin\ProifileControllers;
+Route::Controller(ProfileController::class)->prefix('admin')->group(function() {
+    Route::get('admin/profile/create','add');
+    Route::get('admin/profile/edit','edit');
+});
